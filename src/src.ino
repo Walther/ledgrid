cada8036a13ab7ec85227f73e3ad25dbdc8eb6b5
Veeti Haapsamo <veeti.haapsamo@gmail.com>
2018-01-14 22:57:23 +0200

    Re-route the wires in software, making the physical wiring easier

--- src/src.ino
+++ src/src.ino
@@ -1,9 +1,13 @@
-#define NELEMS(x)  (sizeof(x) / sizeof((x)[0])) // array element counter
+#define elements(x)  (sizeof(x) / sizeof((x)[0]))
+// array element counter
 
-const int ledPins[] = {0,1,2,3,4,5,6,7}; // volt pins are y-axis aka rows
-const int groundPins[] = {14,15,16,17,18,19,20,21}; // groundpins are x-axis aka columns
-const int ledPinCount = NELEMS(ledPins);
-const int groundPinCount = NELEMS(groundPins);
+// row in terms of led matrix pins, positive
+int rows[] = {5, 2, 7, 1, 12, 8, 14, 9};
+// column in terms of led matrix pins, ground
+int columns[] = {13, 3, 4, 10, 6, 11, 15, 16};
+
+const int ledPinCount = elements(rows);
+const int groundPinCount = elements(columns);
 
 int tick = 0;
 int on;
@@ -12,16 +16,33 @@ int off;
 int slowdown = 100;
 // low slowdown = entire grid looks "on",
 // higher slowdown = individual led progression visible
+// try values between 1 and 1000
 
 void setup() {
+  // "post-process" the pins:
+  // pins 1-8 of matrix are at pins 1-8 on teensy
+  // pins 9-16 of matrix are at pins 14-21 on teensy
+  for (int i=0; i<elements(rows); i++){
+    if(rows[i] >= 9) {
+      rows[i] += 5;
+    }
+  }
+  for (int i=0; i<elements(columns); i++){
+    if(columns[i] >= 9) {
+      columns[i] += 5;
+    }
+  }
+
+
   // initialize the "positive volt" pins
-  for(int i=0; i<NELEMS(ledPins);i++) {
-    pinMode(ledPins[i], OUTPUT);
+  for(int i=0; i<elements(rows);i++) {
+    pinMode(rows[i], OUTPUT);
+    digitalWrite(rows[i], LOW);
   }
   // initialize the "programmable ground" pins
-  for(int i=0; i<NELEMS(groundPins);i++) {
-    pinMode(groundPins[i], OUTPUT);
-    digitalWrite(groundPins[i], HIGH);
+  for(int i=0; i<elements(columns);i++) {
+    pinMode(columns[i], OUTPUT);
+    digitalWrite(columns[i], HIGH);
   }
 
 }
@@ -32,13 +53,14 @@ void loop() {
         // PWM 10%
         on = slowdown * 10;
         off = slowdown * 100 - on;
-        
-        digitalWrite(ledPin, HIGH);   // set the LED on
-        digitalWrite(groundPins[groundPin], LOW);
+
+        digitalWrite(rows[ledPin], HIGH);
+        digitalWrite(columns[groundPin], LOW);
         delayMicroseconds(on);
-        digitalWrite(ledPin, LOW);    // set the LED off
+
+        digitalWrite(rows[ledPin], LOW);
+        digitalWrite(columns[groundPin], HIGH);
         delayMicroseconds(off);
-        digitalWrite(groundPins[groundPin], HIGH);
       }
     }
     tick++;
